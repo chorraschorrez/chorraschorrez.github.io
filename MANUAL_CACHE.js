adbc3a65082ac5477f9651812c4ff431e790f1fb
chorraschorrez <156712362+chorraschorrez@users.noreply.github.com>
2024-01-19 19:33:51 +0100

    2024-01-19_1933

--- MANUAL_CACHE.js
+++ MANUAL_CACHE.js
@@ -3,29 +3,8 @@
 
 ===========================
 
-*/
-
-
-/* 
-map @Modo Map
-*/
-let prefijos = ["wonder", "spider", "ant", "iron"]
-let hombre = "man";
-let mujer = "woman";
-
-// CONSOLE SHOWS: ["wonderwoman", "spiderman", "antman", "ironman"]
-let nombresCompletos = prefijos.map(function() { 
-    //return (indice == 0) ? prefijo + mujer : prefijo + hombre;
-    let item;
-    item = item + "man";
- });
-
-console.log(nombresCompletos);
-
-
-
-https://
-www.eni-training.com/cs/ediciones/olr
+@libro online (y pdf)
+https://www.eni-training.com/cs/ediciones/olr
 -Activacion: 
 P9XR67TDC6KW
 
@@ -35,6 +14,31 @@ Mycursojs01.
 
 - Libro en web: 
 https://www.eni-training.com/portal/client/mediabook/home
+
+
+
+*/
+
+
+/* 
+map @Modo Map
+*/
+let Lista = new Array ("palabra1", "palabra2", "palabra3");
+
+// Modo tradicional
+for (let i=0;i<lista.length;i++){
+    lista[i] = convertirAMayusculas(lista[i]);
+};
+console.log(Lista);
+ 
+// Modo Map
+lista = ["palabra1", "palabra2", "palabra3"];
+lista = lista.map(function (){
+    elemento = elemento.toUpperCase();
+    return elemento;
+);
+
+
 
 /* 
 ##### @FILTER
@@ -55,3 +59,24 @@ console.log(diasR);
 
 */
 
+/*
+Ejercicio, cambiar a mayúsculas, coger sólo los de extensión jpg, etc. 
+*/
+let mascotas = ["perrito.jpg","gatito.avi","loro.mp3","galápago.docx","gallo.jpg"];
+
+mascotas = mascotas.map((mascota)=>mascota.toUpperCase());  // Transorm to uppercase
+mascotas = mascotas.filter((mascota) => mascota.endsWith(".JPG"));  // array with only JPG files
+mascotas = mascotas.map( (mascota)=>mascota.slice(0,mascota.lastIndexOf(".")));  // array with file base name, without extension 
+
+console.log(mascotas); //  ['PERRITO', 'GALLO']
+
+
+/*
+Simular, fake web service server (rest, responds json)
+https://app.beeceptor.com/
+Use account from: google gmail
+    chorraschorrez@gmail.com
+    (My,,,01.)Mychorraschorrez01.
+
+- End point: chorras (e.g. chorras.free.app.beeceptor.com   )
+*/ 
--- ejercicio02_slice_map_filter.js
+++ ejercicio02_slice_map_filter.js
@@ -1,14 +1,15 @@
 /*
+Ejercicio, cambiar a mayúsculas, coger sólo los de extensión jpg, etc. 
 */
 let mascotas = ["perrito.jpg","gatito.avi","loro.mp3","galápago.docx","gallo.jpg"];
 
-mascotas = mascotas.map((mascota)=>mascota.toUpperCase());
-mascotas = mascotas.filter((mascota) => mascota.endsWith(".JPG"));
-mascotas = mascotas.map( (mascota)=>mascota.slice(0,mascota.lastIndexOf("."))); 
-console.log(mascotas);
+mascotas = mascotas.map((mascota)=>mascota.toUpperCase());  // Transorm to uppercase
+mascotas = mascotas.filter((mascota) => mascota.endsWith(".JPG"));  // array with only JPG files
+mascotas = mascotas.map( (mascota)=>mascota.slice(0,mascota.lastIndexOf(".")));  // array with file base name, without extension 
 
-mascotas = mascotas.filter((mascota)=>mascota.end);
+console.log(mascotas); //  ['PERRITO', 'GALLO']
 
+/* 
+Reducir resultado 
+*/ 
 
-
-console.log(sItem);
