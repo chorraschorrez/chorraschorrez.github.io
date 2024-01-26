2b4f93db3d56adec8795bcc2f5522d06c5de1e7c
chorraschorrez <156712362+chorraschorrez@users.noreply.github.com>
2024-01-26 20:52:19 +0100

    commit_2024-01-26_2052

--- excepciones_herencia.js
+++ excepciones_herencia.js
@@ -1,12 +1,2 @@
-class ErrorIdentificador extends Error{
-}
-class ErrorIdentificadorFatal extends ErrorIdentificador {
 
-}
 
-try {
-    //throw new ErrorIdentificador("Error de identificador");
-    throw new ErrorIdentificadorFatal("Error de identificador fatal");
-} catch (e){
-    (...)
-} 
