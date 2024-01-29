11e24e5f425de18fa6884f742ae192d298d79529
chorraschorrez <156712362+chorraschorrez@users.noreply.github.com>
2024-01-29 20:47:33 +0100

    commit_2024-01-29_2047

---
+++ .vscode/sesion_14/ejercicio_classlist_1/codigo.js
@@ -0,0 +1,12 @@
+
+const MINIMO=5;
+const OPTIMO=8;
+
+document.querySelector("#identificador").addEventListener("input",()=> {
+    const iNombre = document.querySelector("identificador").ariaValueMax;
+    const nombre = inombres.value;
+    if (nombre.trim().length < MINIMO){
+        inombre.classList.add();
+    }
+}
+);
---
+++ sesion_14/firebase/guasap.js
@@ -0,0 +1,12 @@
+document.querySelector("#bEnviar").onclick = function() {
+    console.log("Leyendo mensaje...")
+    const texto = document.querySelector("#mensaje").value;
+    enviarTexto(texto);
+}
+
+
+//receptor es opcional, por eso lo ponemos así, con valor por defecto: EVERYBODY 
+function enviarTexto(texto, emisor, receptor="EVERYBODY"){
+    console.log("Enviando texto...:" + texto);
+
+}
